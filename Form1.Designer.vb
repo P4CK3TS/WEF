e0ae763a038c445b22d31f5bd8c55d8277298bfb
Kurtis Palmateer <Kurtis Palmateer@PF3WTB9Y>
2023-11-06 20:36:37 -0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,304 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        GroupBox1 = New GroupBox()
+        Button2 = New Button()
+        Label2 = New Label()
+        Label1 = New Label()
+        TextBox2 = New TextBox()
+        Button1 = New Button()
+        TextBox1 = New TextBox()
+        RadioButton3 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        RadioButton1 = New RadioButton()
+        MenuStrip1 = New MenuStrip()
+        FileToolStripMenuItem = New ToolStripMenuItem()
+        ImportToolStripMenuItem = New ToolStripMenuItem()
+        ExportToolStripMenuItem = New ToolStripMenuItem()
+        AuthorToolStripMenuItem = New ToolStripMenuItem()
+        HelpToolStripMenuItem = New ToolStripMenuItem()
+        StatusStrip1 = New StatusStrip()
+        ToolStripStatusLabel1 = New ToolStripStatusLabel()
+        GroupBox2 = New GroupBox()
+        ListBox1 = New ListBox()
+        GroupBox3 = New GroupBox()
+        ListBox2 = New ListBox()
+        OpenFileDialog1 = New OpenFileDialog()
+        GroupBox1.SuspendLayout()
+        MenuStrip1.SuspendLayout()
+        StatusStrip1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        GroupBox3.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(Button2)
+        GroupBox1.Controls.Add(Label2)
+        GroupBox1.Controls.Add(Label1)
+        GroupBox1.Controls.Add(TextBox2)
+        GroupBox1.Controls.Add(Button1)
+        GroupBox1.Controls.Add(TextBox1)
+        GroupBox1.Controls.Add(RadioButton3)
+        GroupBox1.Controls.Add(RadioButton2)
+        GroupBox1.Controls.Add(RadioButton1)
+        GroupBox1.Location = New Point(12, 26)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(672, 72)
+        GroupBox1.TabIndex = 0
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Target Options"
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(449, 37)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 8
+        Button2.Text = "Browse"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(320, 20)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(40, 15)
+        Label2.TabIndex = 7
+        Label2.Text = "Range"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(8, 20)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(45, 15)
+        Label1.TabIndex = 6
+        Label1.Text = "Host IP"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Enabled = False
+        TextBox2.Location = New Point(320, 38)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(42, 23)
+        TextBox2.TabIndex = 5
+        TextBox2.Text = "/32"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(368, 38)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 4
+        Button1.Text = "Add"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(8, 38)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(306, 23)
+        TextBox1.TabIndex = 3
+        TextBox1.Text = "127.0.0.1"
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.AutoSize = True
+        RadioButton3.Location = New Point(595, 12)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(71, 19)
+        RadioButton3.TabIndex = 2
+        RadioButton3.TabStop = True
+        RadioButton3.Text = "IP Range"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(518, 12)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(71, 19)
+        RadioButton2.TabIndex = 1
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "Host List"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Checked = True
+        RadioButton1.Location = New Point(427, 12)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(85, 19)
+        RadioButton1.TabIndex = 0
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "Single Host"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.Items.AddRange(New ToolStripItem() {FileToolStripMenuItem, ImportToolStripMenuItem, ExportToolStripMenuItem, AuthorToolStripMenuItem, HelpToolStripMenuItem})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(993, 24)
+        MenuStrip1.TabIndex = 1
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' FileToolStripMenuItem
+        ' 
+        FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        FileToolStripMenuItem.Size = New Size(37, 20)
+        FileToolStripMenuItem.Text = "File"
+        ' 
+        ' ImportToolStripMenuItem
+        ' 
+        ImportToolStripMenuItem.Name = "ImportToolStripMenuItem"
+        ImportToolStripMenuItem.Size = New Size(55, 20)
+        ImportToolStripMenuItem.Text = "Import"
+        ' 
+        ' ExportToolStripMenuItem
+        ' 
+        ExportToolStripMenuItem.Name = "ExportToolStripMenuItem"
+        ExportToolStripMenuItem.Size = New Size(53, 20)
+        ExportToolStripMenuItem.Text = "Export"
+        ' 
+        ' AuthorToolStripMenuItem
+        ' 
+        AuthorToolStripMenuItem.Name = "AuthorToolStripMenuItem"
+        AuthorToolStripMenuItem.Size = New Size(52, 20)
+        AuthorToolStripMenuItem.Text = "About"
+        ' 
+        ' HelpToolStripMenuItem
+        ' 
+        HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        HelpToolStripMenuItem.Size = New Size(44, 20)
+        HelpToolStripMenuItem.Text = "Help"
+        ' 
+        ' StatusStrip1
+        ' 
+        StatusStrip1.Items.AddRange(New ToolStripItem() {ToolStripStatusLabel1})
+        StatusStrip1.Location = New Point(0, 631)
+        StatusStrip1.Name = "StatusStrip1"
+        StatusStrip1.Size = New Size(993, 22)
+        StatusStrip1.TabIndex = 2
+        StatusStrip1.Text = "StatusStrip1"
+        ' 
+        ' ToolStripStatusLabel1
+        ' 
+        ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        ToolStripStatusLabel1.Size = New Size(26, 17)
+        ToolStripStatusLabel1.Text = "Idle"
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Controls.Add(ListBox1)
+        GroupBox2.Location = New Point(12, 104)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(672, 524)
+        GroupBox2.TabIndex = 3
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Console"
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 15
+        ListBox1.Location = New Point(6, 18)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(660, 499)
+        ListBox1.TabIndex = 0
+        ' 
+        ' GroupBox3
+        ' 
+        GroupBox3.Controls.Add(ListBox2)
+        GroupBox3.Location = New Point(690, 27)
+        GroupBox3.Name = "GroupBox3"
+        GroupBox3.Size = New Size(291, 601)
+        GroupBox3.TabIndex = 4
+        GroupBox3.TabStop = False
+        GroupBox3.Text = "Target List"
+        ' 
+        ' ListBox2
+        ' 
+        ListBox2.FormattingEnabled = True
+        ListBox2.ItemHeight = 15
+        ListBox2.Location = New Point(5, 20)
+        ListBox2.Name = "ListBox2"
+        ListBox2.Size = New Size(280, 574)
+        ListBox2.TabIndex = 0
+        ' 
+        ' OpenFileDialog1
+        ' 
+        OpenFileDialog1.FileName = "OpenFileDialog1"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(993, 653)
+        Controls.Add(GroupBox3)
+        Controls.Add(GroupBox2)
+        Controls.Add(StatusStrip1)
+        Controls.Add(GroupBox1)
+        Controls.Add(MenuStrip1)
+        MainMenuStrip = MenuStrip1
+        Name = "Form1"
+        Text = "Web Exploiter Frontend Tool (WEFT)"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        StatusStrip1.ResumeLayout(False)
+        StatusStrip1.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        GroupBox3.ResumeLayout(False)
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ImportToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExportToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents AuthorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+End Class
---
+++ Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub AuthorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AuthorToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
+
+    Private Sub StatusStrip1_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs) Handles StatusStrip1.ItemClicked
+
+    End Sub
+End Class
